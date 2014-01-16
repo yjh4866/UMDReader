6412a9e1acb7515405f0cb1fc9c2f65f4c7cf5c7
yjh4866 <yjh4866@163.com>
2014-01-16 22:33:04 +0800

    Initial Commit

---
+++ UMDReader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UMDReader
+//
+//  Created by yangjh on 14-1-16.
+//  Copyright (c) 2014年 yangjh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ UMDReader/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UMDReader
+//
+//  Created by yangjh on 14-1-16.
+//  Copyright (c) 2014年 yangjh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
